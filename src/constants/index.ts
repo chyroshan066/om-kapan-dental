d3c6a2607cafa7075902a26098996be4c12aba14
unknown <chyroshan066@gmail.com>
2026-08-01 20:42:23 +0545

    feat(ui): add responsive Sevices component

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export * from "./structured-data";
 export * from "./nav-links";
 export * from "./social-links";
 export * from "./contacts";
+export * from "./services";
---
+++ src/constants/services.ts
@@ -0,0 +1,22 @@
+export const SERVICES = [
+    {
+        name: "Dentures",
+        img: "images/services/dentures.webp",
+        text: "Lorem ipsum is placeholder text commonly used in the graphic,",
+    },
+    {
+        name: "Implants",
+        img: "images/services/implants.webp",
+        text: "Lorem ipsum is placeholder text commonly used in the graphic,",
+    },
+    {
+        name: "Whitening",
+        img: "images/services/whiting.webp",
+        text: "Lorem ipsum is placeholder text commonly used in the graphic,",
+    },
+    {
+        name: "Root Canal",
+        img: "images/services/rootcanal.webp",
+        text: "Lorem ipsum is placeholder text commonly used in the graphic,",
+    },
+];
